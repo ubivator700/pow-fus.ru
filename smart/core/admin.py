43b39b7656f99e6f16da4e2574e07c57b838a0a7
Nikita Sorogi <nikita.sorogi@gmail.com>
2023-02-18 21:25:54 +0300

    edit: user model

--- smart/core/admin.py
+++ smart/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Users, Companies
 
-# Register your models here.
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    exclude=('user_id',)
+
+@admin.register(Companies)
+class CompaniesAdmin(admin.ModelAdmin):
+    exclude=('company_id',)
--- smart/core/models.py
+++ smart/core/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
 
+USER_ROLE_CHOICE = [
+    ('director', 'Директор'),
+    ('top-manager', 'Топ-менеджер'),
+    ('manager','Менеджер')
+]
+
+
 class Companies(models.Model):
-    company_id = models.IntegerField(auto_created=True, blank=False)
+    company_id = models.AutoField(primary_key=True)
     company_name = models.CharField(max_length=200, blank=True, default='')
 
     class Meta:
         ordering = ['company_id']
 
+    def __str__(self):
+        return self.company_name
+
 
 class Users(models.Model):
-    user_id = models.IntegerField(auto_created=True, blank=False)
-    user_name = models.CharField(max_length=200, blank=False, default='Пользователь')
-    user_role = models.CharField(max_length=100, default='user')
-    user_photo = models.ImageField()
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, blank=False, default='Пользователь')
     company_name = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    avatar = models.ImageField()
+    role = models.TextField(choices=USER_ROLE_CHOICE, default='manager')
 
     class Meta:
-        ordering = ['user_id']
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
